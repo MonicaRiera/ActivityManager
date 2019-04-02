15def9d8429ddcf337467d09186b5c9e04f8d39a
Monica <monica.riera@bts.tech>
2019-04-02 13:04:54 +0200

    Connect 'Button' widget to 'btnLoad'

--- app/src/main/java/com/example/activitymanager/MainActivity.java
+++ app/src/main/java/com/example/activitymanager/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLoad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "onCreate()", Toast.LENGTH_SHORT).show();
+
+        btnLoad = findViewById(R.id.activity_main__btn__load);
+        btnLoad.setText("Hello");
     }
 
     @Override
